d61e60fd3e69a72e7ac4e941e88c272635236d6d
jhkim <coll1994@naver.com>
2024-08-10 20:46:56 +0900

    Nomad_Coders/1_MEME-MAKER :: 1.2. Paths
    - cavnas에서 `rect()`와 `fill()`을 이용하여 그림 그리기
    - 색상 변경 방법 학습
    - 각 그림 그릴 때는 경로(Path) 단위로 구분되는 것을 확인

--- Nomad_Coders/1_MEME-MAKER/app.js
+++ Nomad_Coders/1_MEME-MAKER/app.js
@@ -7,4 +7,15 @@ canvas.width =800;
 canvas.height = 800;
 
 // 사각형 그리기
-ctx.fillRect(50, 50, 100, 200)
+// ctx.fillRect(50, 50, 100, 200)
+// 사각형 그리기 (분리)
+ctx.rect(50, 50, 100, 100)
+ctx.rect(150, 150, 100, 100)
+ctx.rect(250, 250, 100, 100)
+ctx.fill();
+
+ctx.beginPath(); // 새 경로 생성하여 기존의 경로와 분리
+ctx.rect(350, 350, 100, 100)
+ctx.rect(450, 450, 100, 100)
+ctx.fillStyle = "red";
+ctx.fill();
