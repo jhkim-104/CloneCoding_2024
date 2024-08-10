43b4a3630975ca113851d6f916a294c49e6c3f0f
jhkim <coll1994@naver.com>
2024-08-10 21:31:29 +0900

    Nomad_Coders/1_MEME-MAKER :: 1.5. Drawing Project Two :: 사람 모양 그리기

--- Nomad_Coders/1_MEME-MAKER/app.js
+++ Nomad_Coders/1_MEME-MAKER/app.js
@@ -6,18 +6,23 @@ const ctx = canvas.getContext("2d") // 2d 이외의 나머지 옵션은 3d를 �
 canvas.width =800;
 canvas.height = 800;
 
-/* 집 모양 그리기 */
-// 벽 그리기
-ctx.fillRect(200, 200, 50, 200); //   왼쪽 벽
-ctx.fillRect(400, 200, 50, 200); // 오른쪽 벽
-// 문 그리기
-ctx.lineWidth = 2; // 문의 두께 변경
-// 중요! : 스타일 변경은 실제 그리기 전에 진행되어야 합니다.
-ctx.fillRect(300, 300, 50, 100); // 문
-// 천장 그리기
-ctx.fillRect(200, 200, 200, 20); // 천장
-// 지붕 그리기 (삼각형)
-ctx.moveTo(200, 200); // 좌측 시작점으로 이동
-ctx.lineTo(325, 100); // 중앙 위쪽으로 그리기
-ctx.lineTo(450, 200); // 우측 종료점까지 그리기
-ctx.fill(); // 지붕 채우기
+/* 사람 모양 그리기 */
+// 몸통 그리기
+ctx.fillRect(210, 200, 15, 100); // 왼쪽 팔
+ctx.fillRect(350, 200, 15, 100); // 오른쪽 팔
+ctx.fillRect(260, 200, 60, 200); // 몸통
+// 얼굴 그리기
+ctx.arc( // `arc()`함수는 많은 매개 변수가 사용됩니다.
+    290,        // x 좌표
+    150,        // y 좌표
+    40,         // 반지름의 크기
+    0,          // 시작 angle :: 0도
+    2 * Math.PI // 끝 angle   :: 360도
+);
+ctx.fill();
+// 눈 그리기
+ctx.beginPath(); // 스타일 변경을 위해 Path 분리
+ctx.fillStyle = "white"; // 색상 변경 시에는 새로운 path 검토하고 적용 전에 스타일 변경해야 합니다.
+ctx.arc(275, 140, 5, 0, 2 * Math.PI);
+ctx.arc(305, 140, 5, 0, 2 * Math.PI);
+ctx.fill();
