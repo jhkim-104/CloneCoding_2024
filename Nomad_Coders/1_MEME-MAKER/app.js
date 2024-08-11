a80c7134d9af12b9e7dea2cb83b74b1030f362db
jhkim <coll1994@naver.com>
2024-08-11 15:17:13 +0900

    Nomad_Coders/1_MEME-MAKER :: 2.4. Paint Color part Two
    - 선의 색상용 버튼을 span을 통해서 생성하였습니다.

--- Nomad_Coders/1_MEME-MAKER/app.js
+++ Nomad_Coders/1_MEME-MAKER/app.js
@@ -1,3 +1,8 @@
+// HTML 측에서 "color-option" 클래스를 갖는 element를 획득하여 array 배열로 사용
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-option") 
+    // 그대로 사용하면 array가 아니라 foreach 등을 사용할 수 없습니다.
+);
 // HTML 측의 input 태그 중 "color" element에 접근
 const color = document.getElementById("color");
 // HTML 측의 input 태그 중 "line-width" element에 접근
@@ -37,6 +42,18 @@ function onColorChange(event) {
     ctx.fillStyle = event.target.value; // 채우기 색상을 설정
 }
 
+function onColorClick(event) {
+    // console.dir(event.target);  // event.target으로 클릭된 element를 확인할 수 있습니다.
+    //                             // console.dir로 element를 상세히 볼 수 있습니다.
+
+    // 선택된 color 획득
+    const colorValue = event.target.dataset.color; 
+
+    ctx.strokeStyle = colorValue; // 선의 색상을 설정
+    ctx.fillStyle = colorValue; // 채우기 색상을 설정
+    color.value = colorValue; // color input의 색상도 변경
+}
+
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -44,3 +61,6 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+
+// 각 color element에 이벤트 리스너 등록
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
