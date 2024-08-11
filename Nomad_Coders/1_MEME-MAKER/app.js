a49a62d2c4af7567f4f7d203b1dd5e211bd47823
jhkim <coll1994@naver.com>
2024-08-11 13:25:13 +0900

    Nomad_Coders/1_MEME-MAKER :: 2.1. Mouse Painting
    - 마우스 눌리고 뗄 때까지 선이 그려지도록 구현

--- Nomad_Coders/1_MEME-MAKER/app.js
+++ Nomad_Coders/1_MEME-MAKER/app.js
@@ -8,26 +8,24 @@ canvas.height = 800;
 // canvas의 선 설정
 ctx.lineWidth = 2;
 
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff"
-]
-
-function onClick(event) {
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+let isPainting = false;
+function onMouseMove(event) {
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting(event) {
+    isPainting = true;
+}
+function cancelPainting(event) {
+    isPainting = false;
 }
 
-// canvas.addEventListener("click", onClick)
-canvas.addEventListener("mousemove", onClick)
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+// document.addEventListener("mouseup", cancelPainting)
