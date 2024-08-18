215e0e8f615ad1475d4c8c24b56c37f55d0a8264
jhkim <coll1994@naver.com>
2024-08-18 20:10:21 +0900

    Nomad_Coders/2_ZOOM :: 0.3 : Frontend Setup
    - Frontend 기본 설정 진행
    - 각 구성 요소 생성 및 연결
    - MVP CSS 적용

--- Nomad_Coders/2_ZOOM/src/server.js
+++ Nomad_Coders/2_ZOOM/src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // "/"만 사용할 것이므로 다른 url 모두 home으로 리다이렉트
 
-app.listen(3000);
+const portNumber = 3000;
+const handleListen = () => console.log(`Listening on http://localhost:${portNumber}`)
+app.listen(portNumber, handleListen);
