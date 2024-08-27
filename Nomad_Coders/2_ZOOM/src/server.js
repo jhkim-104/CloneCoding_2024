a10aabfe45dc0613dde5d52b148a6a4b5ee31550
jhkim <coll1994@naver.com>
2024-08-28 08:11:32 +0900

    Nomad_Coders/2_ZOOM :: 1.3 WebSocket Events
    - backend에서 "connection" 구현
    - frontend에서 webSocket 연결 구현

--- Nomad_Coders/2_ZOOM/src/server.js
+++ Nomad_Coders/2_ZOOM/src/server.js
@@ -7,13 +7,18 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/")); // "/"만 사용할 것이므로 다른 url 모두 home으로 리다이렉트
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/")); // "/"만 사용할 것이므로 다른 url 모두 home으로 리다이렉트
 
 const portNumber = 3000;
 const handleListen = () => console.log(`Listening on http://localhost:${portNumber}`)
 
 const server = http.createServer(app);  // http 패키지 사용해 서버 생성 (with express)
 const wss = new WebSocket.Server({ server });   // ws server 생성
+
+function handleConnection(socket) {
+    console.log(socket)
+}
+wss.on("connection", handleConnection )
 
 server.listen(3000, handleListen);  // server listen
