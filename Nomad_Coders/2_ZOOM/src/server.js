eb09a42d4cef7d75ffff836eec5aac611f64c6a9
jhkim <coll1994@naver.com>
2024-09-01 22:12:36 +0900

    Nomad_Coders/2_ZOOM : 2.11. Admin Panel
    - socket.io의 `admin-ui` 패키지를 이용한 admin 페이지 설정

--- Nomad_Coders/2_ZOOM/src/server.js
+++ Nomad_Coders/2_ZOOM/src/server.js
@@ -1,7 +1,7 @@
 import http from "http" // nodejs에 이미 포함되어 있는 패키지라 설치하지 않아도 됩니다.
 import {Server} from "socket.io"; // socket.io 패키지 import
+const { instrument } = require("@socket.io/admin-ui");
 import express from "express";
-import { count } from "console";
 
 const app = express();
 
@@ -15,7 +15,17 @@ const portNumber = 3000;
 const handleListen = () => console.log(`Listening on http://localhost:${portNumber}`)
 
 const httpServer = http.createServer(app);  // http 패키지 사용해 서버 생성 (with express)
-const wsServer = new Server(httpServer); // socket.io 서버 생성
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"], // socket.io의 데모를 사용하기 위해 cors 허용 처리
+        credentials: true,
+    },
+}); // socket.io 서버 생성
+
+instrument(wsServer, { // admin ui 사용하도록 허용
+  auth: false, // 추후 비밀번호 사용 가능합니다.
+  mode: "development",
+});
 
 function publicRooms() {
     const {
