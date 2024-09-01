263af7487ac49bb7fa74f7782e29719a4f01f636
jhkim <coll1994@naver.com>
2024-09-02 00:25:17 +0900

    Nomad_Coders/2_ZOOM : 3.1. Call Controls
    - stream의 track을 통해 카메라와 마이크의 on/off 기능 구현
    - 카메라 목록 표기 기능 구현

--- Nomad_Coders/2_ZOOM/src/public/js/app.js
+++ Nomad_Coders/2_ZOOM/src/public/js/app.js
@@ -3,19 +3,38 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+async function getCamera() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            cameraSelect.appendChild(option);
+        });
+        console.log(cameras);
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 async function getMedia() {
     try {
         myStream = await navigator.mediaDevices.getUserMedia({
-            // audio: true,
             video: true,
+            audio: true,
           });
         myFace.srcObject = myStream;
+        await getCamera();
     } catch (e) {
         console.log(e);
     }
@@ -23,6 +42,9 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick() {
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
     if (!muted) {
         muteBtn.innerText = "Unmute";
         muted = true;        
@@ -33,6 +55,9 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
     if (cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
