3d77a4f85509b651c8a8a500e2ffbb50ae43b5d5
jhkim <coll1994@naver.com>
2024-09-25 13:37:10 +0900

    Nomad_Coders/3_React_JS_Movie : 7.2. Coin Tracker
    - 코드 챌린지 달러에서 암호화폐로 변환 구현

--- Nomad_Coders/3_react_js_movie/src/App.js
+++ Nomad_Coders/3_react_js_movie/src/App.js
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [coins, setCoins] = useState([]);
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const [money, setMoney] = useState(0);
+	const onChangeCoin = (event) => setSelectedIndex(event.target.value);
+	const onChaneMoney = (event) => setMoney(event.target.value);
+	// const convertMoney = (money) => {};
 	useEffect(() => {
 		fetch("https://api.coinpaprika.com/v1/tickers")
 			.then((response) => response.json())
@@ -11,21 +16,33 @@ function App() {
 				setLoading(false); // 기존 로딩 완료 시 false로 전환
 			});
 	}, []); // 1회만 사용되도록 의존하는 상태를 비웁니다.
+	console.log(coins[selectedIndex]);
 	return (
 		<div>
 			<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
 			{loading ? (
 				<strong>Loading...</strong>
 			) : (
-				<ul>
-					{coins.map((coin) => (
-						// index 대신 coin 데이터 내부의 id를 사용
-						<li key={coin.id}>
-							{coin.name} ({coin.symbol}): $
-							{coin.quotes.USD.price} USD
-						</li>
-					))}
-				</ul>
+				<div>
+					<div>
+						<select onChange={onChangeCoin}>
+							{coins.map((coin, index) => (
+								// index 대신 coin 데이터 내부의 id를 사용
+								<option key={coin.id} value={index}>
+									{coin.name} ({coin.symbol})
+								</option>
+							))}
+						</select>
+					</div>
+					<hr />
+					<div>
+						<input onChange={onChaneMoney} value={money} /> USD
+					</div>
+					<div>
+						{money / coins[selectedIndex].quotes.USD.price}{" "}
+						{coins[selectedIndex].symbol}
+					</div>
+				</div>
 			)}
 		</div>
 	);
