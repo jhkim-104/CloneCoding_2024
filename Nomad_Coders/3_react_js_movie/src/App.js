6c5a6f27e423661b336ee1d4ea7c278ee8e19212
jhkim <coll1994@naver.com>
2024-09-25 13:13:49 +0900

    Nomad_Coders/3_React_JS_Movie : 7.2. Coin Tracker
    - `useEffect()`를 이용한 데이터 로딩 구현

--- Nomad_Coders/3_react_js_movie/src/App.js
+++ Nomad_Coders/3_react_js_movie/src/App.js
@@ -1,7 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-	return <div></div>;
+	const [loading, setLoading] = useState(true);
+	const [coins, setCoins] = useState([]);
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/tickers")
+			.then((response) => response.json())
+			.then((json) => {
+				setCoins(json); // 상태에 데이터 할당
+				setLoading(false); // 기존 로딩 완료 시 false로 전환
+			});
+	}, []); // 1회만 사용되도록 의존하는 상태를 비웁니다.
+	return (
+		<div>
+			<h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+			{loading ? (
+				<strong>Loading...</strong>
+			) : (
+				<ul>
+					{coins.map((coin) => (
+						// index 대신 coin 데이터 내부의 id를 사용
+						<li key={coin.id}>
+							{coin.name} ({coin.symbol}): $
+							{coin.quotes.USD.price} USD
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 }
 
 export default App;
