5ff5e7ac285c48dc8583d81f513543eeac458fbd
jhkim <coll1994@naver.com>
2024-09-28 09:57:56 +0900

    Nomad_Coders/3_React_JS_Movie : 7.7. Publishing
    - 줄 맞춤 변경

--- Nomad_Coders/3_react_js_movie/src/routes/Detail.js
+++ Nomad_Coders/3_react_js_movie/src/routes/Detail.js
@@ -3,38 +3,36 @@ import { useParams } from "react-router-dom";
 import Movie from "../components/Movie";
 
 function Detail() {
-	const [loading, setLoading] = useState(true);
-	const [movie, setMovie] = useState([]);
-	const { id } = useParams();
-	const getMovie = async () => {
-		const json = await (
-			await fetch(
-				`https://nomad-movies.nomadcoders.workers.dev/movies/${id}`
-			)
-		).json();
-		setMovie(json);
-		setLoading(false);
-	};
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies/${id}`)
+    ).json();
+    setMovie(json);
+    setLoading(false);
+  };
 
-	useEffect(() => {
-		getMovie();
-	}, []);
-	return (
-		<div>
-			{loading ? (
-				<h1>Loading...</h1>
-			) : (
-				<div>
-					<Movie
-						id={movie.id}
-						coverImg={movie.poster_path}
-						title={movie.title}
-						summary={movie.overview}
-						genres={movie.genres.map((genre) => genre.name)}
-					/>
-				</div>
-			)}
-		</div>
-	);
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Movie
+            id={movie.id}
+            coverImg={movie.poster_path}
+            title={movie.title}
+            summary={movie.overview}
+            genres={movie.genres.map((genre) => genre.name)}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
