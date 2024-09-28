f1dce11da155fc9118d4d909d56267a482c41a75
jhkim <coll1994@naver.com>
2024-09-28 09:11:39 +0900

    Nomad_Coders/3_React_JS_Movie : 7.7. Publishing
    - 개발 환경에서 route 제대로 동작하지 않는 문제 수정

--- Nomad_Coders/3_react_js_movie/src/routes/Home.js
+++ Nomad_Coders/3_react_js_movie/src/routes/Home.js
@@ -2,43 +2,43 @@ import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
 function Home() {
-	const [loading, setLoading] = useState(true);
-	const [movies, setMovies] = useState([]);
-	const getMovies = async () => {
-		// const response = await fetch(
-		// 	`https://nomad-movies.nomadcoders.workers.dev/movies`
-		// );
-		// const json = await response.json();
-		const json = await (
-			await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies`)
-		).json();
-		setMovies(json);
-		setLoading(false);
-	};
-	useEffect(() => {
-		getMovies();
-	}, []);
-	console.log(movies);
-	return (
-		<div>
-			{loading ? (
-				<h1>Loading...</h1>
-			) : (
-				<div>
-					{movies.map((movie) => (
-						<Movie
-							key={movie.id}
-							id={movie.id}
-							coverImg={movie.poster_path}
-							title={movie.title}
-							summary={movie.overview}
-							genres={movie.genre_ids}
-						/>
-					))}
-				</div>
-			)}
-		</div>
-	);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // const response = await fetch(
+    // 	`https://nomad-movies.nomadcoders.workers.dev/movies`
+    // );
+    // const json = await response.json();
+    const json = await (
+      await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies`)
+    ).json();
+    setMovies(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  // console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              coverImg={movie.poster_path}
+              title={movie.title}
+              summary={movie.overview}
+              genres={movie.genre_ids}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
