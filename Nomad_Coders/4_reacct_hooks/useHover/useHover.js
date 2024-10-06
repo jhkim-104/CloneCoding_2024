3fabe386b5a191f5473fe19db6c279db86548aa3
jhkim <coll1994@naver.com>
2024-10-06 17:04:55 +0900

    Nomad_Coders/4_React_Hooks : 2.3. useConfirm & usePreventLeave
    - useHover 추가
    - useClick과 동작은 동일, 이벤트만 다름

---
+++ Nomad_Coders/4_reacct_hooks/useHover/useHover.js
@@ -0,0 +1,18 @@
+export const useHover = (onHover) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onHover !== "function") {
+      return;
+    }
+    if (element.current) {
+      element.current.addEventListener("mouseenter", onHover);
+    }
+    return () => {
+      if (element.currnet) {
+        element.currnet.removeEventListener("mouseenter", onHover);
+      }
+    };
+  }, []);
+
+  return typeof onHover !== "function" ? undefined : element;
+};
