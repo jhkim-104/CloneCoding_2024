2029e2dd1baac26e099b9b4283cdc63ee1e213e1
jhkim <coll1994@naver.com>
2024-10-06 16:59:49 +0900

    Nomad_Coders/4_React_Hooks : 2.2. useClick
    - useClick 훅 구현
    - useRef 사용

---
+++ Nomad_Coders/4_reacct_hooks/useClick/useClick.js
@@ -0,0 +1,18 @@
+export const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.currnet) {
+        element.currnet.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+
+  return typeof onClick !== "function" ? undefined : element;
+};
