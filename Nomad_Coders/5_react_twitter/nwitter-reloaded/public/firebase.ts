efc7f54e680d3bf690e133019795f5269f82534f
jhkim <coll1994@naver.com>
2024-10-20 23:26:37 +0900

    Nomad_Coders/5_react_twitter : 2.3. Firebase Project
    - 웹에서 Firebase 프로젝트 생성
    - React에 Firebase 통합

---
+++ Nomad_Coders/5_react_twitter/nwitter-reloaded/public/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCs4rTbzh5lJQzCP-5CPyrc-d7iafraGD0",
+  authDomain: "nwitter-reloaded-c3db7.firebaseapp.com",
+  projectId: "nwitter-reloaded-c3db7",
+  storageBucket: "nwitter-reloaded-c3db7.appspot.com",
+  messagingSenderId: "17530630083",
+  appId: "1:17530630083:web:93b51dd61f35fb9fdc9776",
+};
+
+const app = initializeApp(firebaseConfig);
