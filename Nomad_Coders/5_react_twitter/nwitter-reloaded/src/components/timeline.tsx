7a563bc8ae5633b5c9324f24ab00d5828d86fbf7
jhkim <coll1994@naver.com>
2024-10-27 02:02:57 +0900

    Nomad_Coders/5_react_twitter : 4.4. Fetching Timeline
    - home.tsx에서 세로 스크롤 되도록 구현
    - 트윗들 가져오는 기능 구현
    
    Nomad_Coders/5_react_twitter : 4.5. Realtime Tweets
    
    - 실시간 쿼리 변경 사항 수신 기능 구현
    
    Nomad_Coders/5_react_twitter : 4.6. Deleting Tweets
    
    - 트윗 삭제 기능 구현

---
+++ Nomad_Coders/5_react_twitter/nwitter-reloaded/src/components/timeline.tsx
@@ -0,0 +1,81 @@
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  Unsubscribe,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { db } from "../firebase";
+import Tweet from "./tweet";
+
+export interface ITweet {
+  id: string;
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  overflow: scroll;
+`;
+
+export default function TimeLine() {
+  const [tweets, setTweet] = useState<ITweet[]>([]);
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      // const snapshot = await getDocs(tweetsQuery);
+      // const tweets = snapshot.docs.map((doc) => {
+      //   const { tweet, createdAt, userId, username, photo } = doc.data();
+      //   return {
+      //     tweet,
+      //     createdAt,
+      //     userId,
+      //     username,
+      //     photo,
+      //     id: doc.id,
+      //   };
+      // });
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
+    fetchTweets();
+    return () => {
+      // 유저가 컴포넌트를 보지 않을 경우 구독 취소 설정
+      unsubscribe && unsubscribe();
+    };
+  }, []);
+  return (
+    <Wrapper>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
+}
